95e2d39037759065cc979aaf74717832f9d0ba56
IT21191688 <IT21191688@my.sliit.lk>
2023-12-05 22:43:52 +0530

    Login Screen

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -1,18 +1,97 @@
-export const FontFamily = {
-    dMSansBold: "DMSans-Bold",
-    dMSansRegular: "DMSans-Regular",
-    openSansSemiBold: "OpenSans-SemiBold",
-    semibold14: "Poppins-SemiBold",
-    medium14: "Poppins-Medium",
-    bold22: "Poppins-Bold",
-    circularStd: "Circular Std",
-    textXsRegular: "Inter-Regular",
-    archivoExtraBoldItalic: "Archivo-ExtraBoldItalic",
-    displayXsMedium: "Inter-Medium",
-    interSemiBold: "Inter-SemiBold",
-    sFPro: "SF Pro",
-    archivoBlackItalic: "Archivo-BlackItalic",
-    sFProText: "SF Pro Text",
-    archivoBold: "Archivo-Bold",
-    sFProDisplay: "SF Pro Display",
+import * as Font from 'expo-font';
+
+export const customFonts = {
+    DMSansBold: require('./assets/fonts/DMSans-Bold.ttf'),
+    DMSansRegular: require('./assets/fonts/DMSans-Regular.ttf'),
+    'OpenSans-SemiBold': require('./assets/fonts/OpenSans-SemiBold.ttf'),
+    'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Archivo-ExtraBoldItalic': require('./assets/fonts/Archivo-ExtraBoldItalic.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+    'Archivo-BlackItalic': require('./assets/fonts/Archivo-BlackItalic.ttf'),
+    'Archivo-Bold': require('./assets/fonts/Archivo-Bold.ttf'),
+    // Your font definitions here...
 };
+
+export async function loadCustomFonts() {
+    try {
+        await Font.loadAsync(customFonts);
+        //console.log('Custom fonts loaded successfully');
+    } catch (error) {
+        //console.error('Error loading custom fonts: ', error);
+    }
+}
+
+/* font sizes */
+export const FontSize = {
+    size_11xl: 30,
+    medium12_size: 12,
+    medium14_size: 14,
+    semibold16_size: 16,
+    size_21xl: 40,
+    bold22_size: 22,
+    size_mini: 15,
+    medium13_size: 13,
+    size_4xs: 9,
+    size_3xs: 10,
+    displayXsMedium_size: 24,
+    size_base_5: 16,
+    size_xl: 20,
+};
+/* Colors */
+export const Color = {
+    colorWhitesmoke: "#f9f9f9",
+    colorMidnightblue_100: "#130160",
+    colorMidnightblue_300: "#150b3d",
+    colorMidnightblue_200: "#0d0140",
+    colorDimgray: "#524b6b",
+    colorDarkorange: "#ff9228",
+    pureWhite: "#fff",
+    colorWhite: "#fff",
+    colorDarkgray_100: "#aaa6b9",
+    colorDarkgray_300: "#979799",
+    colorDarkgray_200: "#95969d",
+    colorLavender: "#e6e1ff",
+    colorBlack: "#000",
+    greenPure: "#0e9d57",
+    colorGray_100: "#fafafa",
+    colorGray: "#222",
+    gray900: "#101828",
+    black: "#0d0d26",
+    bG: "#fafafd",
+    colorRed: "#ff0000",
+    colorRoyalblue: "#2e6bf1",
+    colorTomato: "#de4619",
+    colorSteelblue: "#356899",
+    colorGainsboro: "#e6e6e6",
+};
+/* Paddings */
+export const Padding = {
+    p_xl: 20,
+    p_5xs: 8,
+    p_6xl: 25,
+    p_5xl: 24,
+    p_29xl: 48,
+    p_base: 16,
+    p_xs: 12,
+    p_11xs: 2,
+    p_9xs: 4,
+};
+/* border radiuses */
+export const Border = {
+    br_7xs: 6,
+    br_8xs: 5,
+    br_3xs: 10,
+    br_78xl: 97,
+    br_xl: 20,
+    br_21xl_4: 40,
+    br_mini: 15,
+    br_lg: 18,
+    br_5xl: 24,
+    br_mid_9: 18,
+    br_sm_8: 14,
+};
+
--- screens/Login.js
+++ screens/Login.js
@@ -1,50 +1,386 @@
-import React, { useState } from "react";
-import { Image } from "expo-image";
-import axios from 'axios';
-import { StyleSheet, Pressable, Text, TextInput, View, TouchableOpacity, Alert } from "react-native";
-//rimport fonts from "../assets/fonts"
-//import { CheckBox } from 'react-native-elements';
-//import { useNavigation } from "@react-navigation/native";
-//import { FontFamily } from "../GlobalStyles";
-//import AsyncStorage from '@react-native-async-storage/async-storage';
-
-
-
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Pressable, Text, TextInput, View, TouchableOpacity, Alert, Image } from "react-native";
+import { loadCustomFonts, customFonts } from "../GlobalStyles"; // Import the loadCustomFonts function and customFonts object
+import { Color, FontFamily, FontSize, Border } from "../GlobalStyles";
+import { CheckBox } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
 const Login = () => {
 
+    /*
+    const [fontsLoaded, setFontsLoaded] = useState(false);
 
+    useEffect(() => {
+        async function loadFonts() {
+            await loadCustomFonts();
+            setFontsLoaded(true);
+            //console.log(fontsLoaded)
+        }
+
+        loadFonts();
+    }, []);
+
+    if (!fontsLoaded) {
+        return null; // You can render a loading indicator here
+    }
+
+    */
+
+
+    const navigation = useNavigation();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
+
+
+    const handleSignUpPress = () => {
+        navigation.navigate("SignUp");
+    };
+
+
+    const handleLoginPress = async () => {
+        try {
+            const response = await axios.post('https://uee123.onrender.com/api/v1/auth/login', {
+                email,
+                password,
+            });
+
+            if (response.status === 200) {
+                const { data } = response.data;
+
+                if (data.token) {
+                    // Store the token and role in AsyncStorage or a similar storage mechanism
+                    await AsyncStorage.setItem('token', data.token);
+                } else {
+                    // Handle the case where the token is missing or undefined
+                    console.error('Token is missing or undefined');
+                }
+
+                if (data.user && data.user.role) {
+                    await AsyncStorage.setItem('role', data.user.role);
+                    // Navigate to the appropriate screen based on the user's role
+                    if (data.user.role === 'admin') {
+                        Alert.alert('Success', 'Login successful');
+                        navigation.navigate('AdminHomePage');
+                        //navigation.navigate('UserHomePage')
+                    } else {
+                        Alert.alert('Success', 'Login successful');
+                        navigation.navigate('UserHomePage');
+                    }
+                } else {
+                    // Handle the case where the user role is missing or undefined
+                    console.error('User role is missing or undefined');
+                }
+
+                // Display a success alert
+
+            } else {
+                // Display an error alert and log the error
+                Alert.alert('Unsuccessful', 'Login Unsuccessful');
+                console.error('Login failed:', response.statusText);
+            }
+        } catch (error) {
+            // Display an error alert and log the error
+            Alert.alert('Unsuccessful', 'Login Unsuccessful');
+            console.error('Login error:', error);
+        }
+    };
+
+
+    const fogotPassword = () => {
+
+
+
+
+    }
 
 
     return (
-        <>
+        <View style={styles.login}>
 
-            <View>
-                <Text style={styles.customText}>Welcome</Text>
+            {/* Welcome Back text */}
+            <Text style={[styles.welcomeBack, styles.email1Typo]}>Welcome</Text>
+
+            {/* Additional text */}
+            <Text style={[styles.loremIpsumDolor, styles.passwordTypo]}>
+                SASIP S.M.S
+            </Text>
+
+            {/* Email Input */}
+            <View style={[styles.email, styles.emailPosition]}>
+                <Image
+                    style={[styles.passwordChild, styles.passwordChildPosition]}
+                    contentFit="cover"
+                    source={require("../assets/rectangle-59.png")}
+                />
+                <TextInput
+                    style={[styles.brandonelouisgmailcom, styles.passwordTypo]}
+                    placeholder="hello@gmail.com"
+                    placeholderTextColor="rgba(13, 1, 64, 0.6)"
+                    onChangeText={text => setEmail(text)}
+                />
+                <Text style={[styles.email1, styles.email1Typo]}>Email</Text>
+            </View>
+
+            {/* Password Input */}
+            <View style={[styles.password, styles.emailPosition]}>
+                <Image
+                    style={[styles.passwordChild, styles.passwordChildPosition]}
+                    contentFit="cover"
+                    source={require("../assets/rectangle-59.png")}
+                />
+                <TextInput
+                    style={[styles.password2, styles.passwordTypo]}
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    placeholderTextColor="rgba(13, 1, 64, 0.6)"
+                    onChangeText={text => setPassword(text)}
+                />
+                <Text style={[styles.password1, styles.passwordClr]}>Password</Text>
+            </View>
+
+            {/* Remember Me Checkbox */}
+            <View style={[styles.rememberMe, styles.iconEyeLayout]}>
+                <CheckBox
+                    style={[styles.rememberMeChild, styles.childShadowBox]}
+                    checked={rememberMe}
+                    onPress={() => setRememberMe(!rememberMe)}
+                    checkedColor="#e6e1ff"
+                    containerStyle={styles.rectangleCheckboxLayout}
+                />
+                <Text style={[styles.rememberMe1, styles.login1Position]}>
+                    Remember me
+                </Text>
             </View>
 
 
+            {/* Forgot Password */}
+            <TouchableOpacity style={[styles.forgotPassword, styles.passwordTypo]} onPress={fogotPassword}>
+
+                <Text >
+                    Forgot Password?
+                </Text>
+
+            </TouchableOpacity>
+
+
+            {/* Login Button */}
+            <TouchableOpacity style={[styles.save, styles.savePosition]} onPress={handleLoginPress}>
+                <View style={[styles.saveChild, styles.savePosition]} />
+                <Text style={[styles.login1, styles.login1Position]}>Login</Text>
+            </TouchableOpacity>
 
 
 
-
-
-        </>
-
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-
-    customText: {
-        color: '#F56513',
-        // fontFamily: FontFamily.dMSansBold,
-        fontSize: 30,
-        fontStyle: 'normal',
-        fontWeight: '700',
-        lineHeight: 'normal',
+    rectangleCheckboxLayout: {
+        backgroundColor: "transparent",
+        padding: 0,
+        left: 0,
+        marginTop: -12,
+        top: "50%",
+        position: "absolute",
     },
-
+    emailPosition: {
+        height: 76,
+        width: 317,
+        marginLeft: -158.5,
+        left: "50%",
+        top: "50%",
+        position: "absolute",
+    },
+    passwordChildPosition: {
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+    },
+    iconEyeLayout: {
+        height: 24,
+        position: "absolute",
+    },
+    passwordClr: {
+        color: Color.colorMidnightblue_200,
+        textAlign: "left",
+        position: "absolute",
+    },
+    passwordTypo: {
+    },
+    email1Typo: {
+        fontWeight: "700",
+    },
+    childShadowBox: {
+        shadowOpacity: 1,
+        elevation: 62,
+        shadowRadius: 62,
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowColor: "rgba(153, 171, 198, 0.18)",
+    },
+    login1Position: {
+        marginTop: -8,
+        textAlign: "left",
+        top: "60%",
+        position: "absolute",
+        alignItems: 'center'
+    },
+    savePosition: {
+        height: 50,
+        width: 266,
+        left: "50%",
+        top: "50%",
+        position: "absolute",
+    },
+    passwordChild: {
+        marginTop: -70,
+        marginLeft: -187.5,
+        borderRadius: Border.br_3xs,
+        width: 375,
+        height: 174,
+        left: "50%",
+        top: "50%",
+    },
+    iconEye: {
+        top: 39,
+        left: 282,
+        width: 24,
+    },
+    password1: {
+        fontWeight: "600",
+        textAlign: "left",
+        left: 3,
+        top: 0,
+        color: Color.colorMidnightblue_200,
+    },
+    password2: {
+        top: 38,
+        width: 240,
+        left: 15,
+        position: "absolute",
+    },
+    password: {
+        marginTop: -61,
+    },
+    brandonelouisgmailcom: {
+        top: 38,
+        left: 15,
+        width: 285,
+        position: "absolute",
+    },
+    email1: {
+        textAlign: "left",
+        color: Color.colorMidnightblue_200,
+        position: "absolute",
+        fontSize: FontSize.size_xs,
+        left: 3,
+        top: 0,
+    },
+    email: {
+        marginTop: -152,
+    },
+    rememberMeChild: {
+        borderRadius: Border.br_8xs,
+        backgroundColor: Color.colorLavender,
+    },
+    rememberMe1: {
+        left: 40,
+        color: Color.colorDarkgray_100,
+        fontSize: FontSize.size_mini,
+    },
+    rememberMe: {
+        marginTop: 35,
+        left: 32,
+        width: 121,
+        top: "50%",
+    },
+    saveChild: {
+        marginTop: -25,
+        marginLeft: -133,
+        borderRadius: Border.br_7xs,
+        backgroundColor: '#F56513',
+        shadowOpacity: 1,
+        elevation: 62,
+        shadowRadius: 62,
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowColor: "rgba(153, 171, 198, 0.18)",
+    },
+    login1: {
+        fontSize: FontSize.size_sm,
+        letterSpacing: 0.8,
+        textTransform: "uppercase",
+        color: Color.colorWhite,
+        fontWeight: "700",
+        left: 100,
+        textAlign: 'center'
+    },
+    save: {
+        marginTop: 91,
+        marginLeft: -130.5,
+    },
+    forgotPassword: {
+        marginTop: 39,
+        right: 21,
+        textAlign: "left",
+        color: Color.colorMidnightblue_200,
+        position: "absolute",
+        top: "50%",
+    },
+    youDontHave: {
+        color: Color.colorDimgray,
+    },
+    signUp: {
+        textDecoration: "underline",
+        color: Color.colorDarkorange,
+    },
+    text: {
+        marginTop: 173,
+        marginLeft: -110.5,
+        textAlign: "left",
+    },
+    youDontHaveContainer: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        textAlign: 'center',
+        // Other styles...
+    },
+    loremIpsumDolor: {
+        marginTop: -254,
+        marginLeft: -145.5,
+        lineHeight: 19,
+        textAlign: "center",
+        width: 291,
+        color: Color.colorDimgray,
+        left: "50%",
+        top: "50%",
+        position: "absolute",
+    },
+    welcomeBack: {
+        marginTop: -308,
+        marginLeft: -70,
+        fontSize: FontSize.size_11xl,
+        textAlign: "left",
+        color: "#F56513",
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+    },
+    login: {
+        backgroundColor: Color.colorWhite,
+        flex: 1,
+        width: "100%",
+        height: 812,
+        top: 40,
+        overflow: "hidden",
+    },
 });
-
 
 export default Login;
